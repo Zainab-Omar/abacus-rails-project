e4d7d410fcb19f322686de4f478d9ca08b39c01f
enciie <enciie@gmail.com>
2018-11-20 00:36:17 -0800

    Update create form to render on initial page load

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,17 +1,28 @@
 $(document).on('turbolinks:load', function(){
+  attachListeners();
+})
 
-  $("a#new_group_form").on("click", function(event){
-    //Fire some AJAX
-    $.ajax({
-      method: "GET",
-      url: this.href,
-      ///groups/new
-    }).success(function(response) {
-      //chain a callback function on what to when we're done
-      $("div.group_form").html(response)
-      $("a#new_group_form").hide();
-    });
-    event.preventDefault();
+const attachListeners = function() {
+
+  $.get("/groups/new", function(response) {
+  }).success(function(response) {
+    $("div.group_form").html(response)
   })
 
-})
+  // Get Create Group Form via Ajax
+  // $("a#new_group_form").on("click", function(event){
+  //   //Fire some AJAX
+  //   $.ajax({
+  //     method: "GET",
+  //     url: this.href,
+  //     ///groups/new
+  //   }).success(function(response) {
+  //     //chain a callback function on what to when we're done
+  //     $("div.group_form").html(response)
+  //     $("a#new_group_form").hide();
+  //   });
+  //   event.preventDefault();
+  // })
+
+}
+//end of attachListeners
