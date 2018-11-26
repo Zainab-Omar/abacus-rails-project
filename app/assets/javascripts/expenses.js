903d9856c07eb3aaaf10e6c61bc8c7534396d0fb
enciie <enciie@gmail.com>
2018-11-26 01:32:54 -0800

    update json data for groups show page to groups/:id.json

--- app/assets/javascripts/expenses.js
+++ app/assets/javascripts/expenses.js
@@ -124,8 +124,9 @@ var attachListeners = function() {
 // Loads all expenses
 function loadExpenses(){
   let url = this.location.href
-  url += "/expenses.json"
+  url += ".json"
   $.get(url, function(json){
+    debugger
     updateTableHtml(json)
   })
   //end of get call
@@ -168,18 +169,19 @@ function loadExpenses(){
     let amount = "";
     let total = 0;
     let totalHTML = "";
+    let groupId = json.id
+    let groupExpenses = json.expenses
     //iterate over each expense within json
-    if (json.length) {
+    if (groupExpenses.length) {
       //checks if the json array is empty
-      json.forEach(function(expense){
+      groupExpenses.forEach(function(expense){
         // expense => {id: 210, description: "5", amount: 5, created_at: "2018-11-26T03:57:56.291Z", category: {…}, …}amount: 5category: {name: "Gifts"}created_at: "2018-11-26T03:57:56.291Z"description: "5"group: {id: 159, name: "5"}id: 210__proto__: Object
         let $table = $("#groups-exp")
         let trHTML = "";
-
         trHTML += '<tr><td>' + expense.description + '</td><td> $' + expense.amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,") + '</td><td>'
         trHTML += formatDate(expense.created_at) + '</td><td>' + expense.category.name + '</td>'
-        trHTML += '<td>' + `<a class="glyphicon glyphicon-pencil" id="pencil-icon" href="/groups/${expense.group.id}/expenses/${expense.id}/edit">` + '</td>'
-        trHTML += '<td>' + `<a data-confirm="Are you sure?" class="glyphicon glyphicon-trash" id="trash-icon" rel="nofollow" data-method="delete" href="/groups/${expense.group.id}/expenses/${expense.id}"></a>` + '</td></tr>';
+        trHTML += '<td>' + `<a class="glyphicon glyphicon-pencil" id="pencil-icon" href="/groups/${groupId}/expenses/${expense.id}/edit">` + '</td>'
+        trHTML += '<td>' + `<a data-confirm="Are you sure?" class="glyphicon glyphicon-trash" id="trash-icon" rel="nofollow" data-method="delete" href="/groups/${groupId}/expenses/${expense.id}"></a>` + '</td></tr>';
         $table.append(trHTML)
         amount = parseFloat(expense.amount)
         total += amount
